3850be85a818d1c938d339d4a6e69c11791ba70f
PelteaLeonard <94557659+PelteaLeonard@users.noreply.github.com>
2022-04-26 17:30:42 +0100

    commit on master

--- static/js/script.js
+++ static/js/script.js
@@ -17,8 +17,8 @@ if(close){
 }
 
 
-var MainImg = document.getElementById("MainImg");
-var smallimg = document.getElementsByClassName("small-img");
+const MainImg = document.getElementById("MainImg");
+const smallimg = document.getElementsByClassName("small-img");
 
 smallimg[0].onclick = function(){
     MainImg.src = smallimg[0].src;
@@ -41,7 +41,7 @@ function closeModal() {
     document.getElementById("myModal").style.display = "none";
 }
   
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
   
 function plusSlides(n) {
@@ -53,15 +53,14 @@ function currentSlide(n) {
 }
   
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("demo");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
+    for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
@@ -74,3 +73,25 @@ const shoppingCart = document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick = () =>{
     shoppingCart.classList.toggle('active');
 }
+
+document.querySelector('.minus-btn').setAttribute("disabled", "disabled");
+
+var valueCount
+document.querySelector(".plus-btn").addEventListener("click", function(){
+    valueCount = document.getElementById("quantity").value;
+    valueCount++;
+    document.getElementById("quantity").value = valueCount;
+    if(valueCount >0){
+        document.querySelector(".minus-btn").removeAttribute("disabled");
+        document.querySelector(".minus-btn").classList.remove("disabled");
+    }
+});
+
+document.querySelector(".minus-btn").addEventListener("click", function(){
+    valueCount = document.getElementById("quantity").value;
+    valueCount--;
+    document.getElementById("quantity").value = valueCount;
+    if(valueCount ==0){
+        document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
+    }
+});
